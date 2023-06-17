28702315bce3249d6a4135fa7bca8874bc38dcdd
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-06-17 17:54:32 +0900

    Feat res JSON Data

--- UNO/link_terminal/link_terminal.ino
+++ UNO/link_terminal/link_terminal.ino
@@ -1,6 +1,8 @@
 // UNO
 
 #include <SoftwareSerial.h>
+#include <string.h>
+#include <ArduinoJson.h>
 
 SoftwareSerial ss(6, 7);
 
@@ -17,6 +19,31 @@ void loop() {
 
   if (ss.available()) {
     // MEGA2560 -> UNO
-    Serial.write(ss.read());
+    String json;
+    // 수신된 데이터를 String으로 수집
+    while (ss.available()) {
+      json += (char)ss.read();
+      delay(1);
+    }
+
+    const size_t CAPACITY = JSON_OBJECT_SIZE(100);
+    StaticJsonDocument<CAPACITY> doc;
+    DeserializationError error = deserializeJson(doc, json);
+
+    if (error) {
+      Serial.print("Deserialization error: ");
+      Serial.println(error.c_str());
+      return;
+    }
+    const char* id = doc["Id"];
+    int price = doc["Price"];
+    int count = doc["Count"];
+
+    Serial.print("Id: ");
+    Serial.println(id);
+    Serial.print("Price: ");
+    Serial.println(price);
+    Serial.print("Count: ");
+    Serial.println(count);
   }
 }
