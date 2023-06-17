fc40b146a682c9062a76440eb6a6bcafd8c36677
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-06-18 02:46:30 +0900

    Feat Uno Link Terminal
    - Serial을 이용하여 제어 코드 전송 부분 작성

--- UNO/link_terminal/link_terminal.ino
+++ UNO/link_terminal/link_terminal.ino
@@ -4,11 +4,30 @@
 #include <string.h>
 #include <ArduinoJson.h>
 
+#define BTN 2
+
 SoftwareSerial ss(6, 7);
+
+char* req = "http -a admin:admin get :3000/item/8808024031923 --pretty=none --print=b";
+
+
+void sendWire(char ch) {
+  Serial.print(ch);
+  ss.write(ch);
+}
+
+void sendWire(const char* str) {
+  for (int i = 0; i < strlen(str); i++) {
+    sendWire(str[i]);
+  }
+  sendWire('\n');
+}
 
 void setup() {
   Serial.begin(9600);
   ss.begin(9600);
+
+  pinMode(BTN, INPUT);
 }
 
 void loop() {
@@ -17,9 +36,16 @@ void loop() {
     ss.write(Serial.read());
   }
 
+  if(digitalRead(BTN)) {
+    Serial.println("btn");
+    sendWire(req);
+    delay(100);
+  }
+
   if (ss.available()) {
     // MEGA2560 -> UNO
     String json;
+    
     // 수신된 데이터를 String으로 수집
     while (ss.available()) {
       json += (char)ss.read();
